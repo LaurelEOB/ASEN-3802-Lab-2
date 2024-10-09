edfb6f0298741080b0588d4446b292b4d9d6322c
Laurel O'Brien <laurelobrien126@gmail.com>
2024-10-08 20:20:47 -0600

    added H_an

--- Main.m
+++ Main.m
@@ -1,9 +1,9 @@
 close all; clear; clc;
 
 %% Constants
-d_rod = 1*0.0254; % Diameter of rod, 1m
-A_rod = 2*pi*d_rod/2; % Cross section of the rod, m^2
-k = [130,130,115,115,16.2]; % Thermal Conductivity (k) [W/(m*K)];
+d_rod = 1*0.0254; % Diameter of rod, [m]
+A_rod = 2*pi*d_rod/2; % Cross section of the rod, [m^2]
+k = [130,130,115,115,16.2]; % Thermal Conductivity (k) [W/(m*K)]=[W/(m*C)];
 
 filename(1) = "Aluminum_21V_203mA.csv";
 filename(2) = "Aluminum_30V_290mA.csv";
@@ -12,38 +12,43 @@ filename(4) = "Brass_30V_285mA.csv";
 filename(5) = "Steel_21V_194mA.csv";
 
 %% Go through each file
-for j=1:1%length(filename)
+for i=1:length(filename)
     figure();
     hold on;
     set(gca,"ColorOrder",jet(25))
 
-    titleFile = char (filename(j)); % Filename for the data
+    titleFile = char (filename(i)); % Filename for the data
     % Voltage and Current
-    if (j==1 || j==2)
-        volt = titleFile(1,10:11);
-        curr = titleFile(1,14:16);
+    if (i==1 || i==2)
+        volt = titleFile(1,10:11); % [V]
+        curr = titleFile(1,14:16); % [mA]
         title(titleFile(1,1:8)+" "+volt+"V, "+curr+"mA");
     else
-        volt = titleFile(1,7:8);
-        curr = titleFile(1,11:13);
+        volt = titleFile(1,7:8); % [V]
+        curr = titleFile(1,11:13); % [mA]
         title(titleFile(1,1:5)+" "+volt+"V, "+curr+"mA");
     end
 
-    rawData = importdata(filename(j));
+    rawData = importdata(filename(i));
     testData = rawData.data;
     plot(testData(:,1),testData(:,2:end));
+    xlabel("time [s]");
+    ylabel("temperature ["+char(176)+"C]")
     legend("Ch1","Ch2","Ch3","Ch4","Ch5","Ch6","Ch7","Ch8");
 
 
-    T_F = zeros(1);
     for j=2:9
         T_F(1,j-1)=testData(end,j);
     end
-    pos_therm = linspace(7.62/100,16.51/100,8);
+    x_0 = (1+3/8)*0.0254;% Distance from x_0 to first thermocouple
+    spacing = 0.5*0.0254; % Distance between thermocouples
+    pos_therm = linspace(x_0,x_0+(8*spacing),8); % [m]
 
     Coeff = polyfit(pos_therm,T_F,1);
-    H_exp = Coeff(1);
-    T_0 = Coeff(2);
+    H_exp(i) = Coeff(1)/k(i)/A_rod;
+    T_0(i) = Coeff(2);
+    %%% Units!!
+    H_an = str2num(volt)*str2num(curr)*(10^-3)/k(i)/A_rod;
 
     hold off;
 end
