b63aa99098ef153ddcaa7c7892344a8317cc237a
Laurel O'Brien <laurelobrien126@gmail.com>
2024-10-14 17:03:54 -0600

    Got rid of temp over time plots and changed to steady state

--- Main.m
+++ Main.m
@@ -12,15 +12,16 @@ filename(4) = "Brass_30V_285mA.csv";
 filename(5) = "Steel_21V_194mA.csv";
 figure('Position', [40 60 1100 700]); hold on;
 t2 = tiledlayout(2,3);
-t2.TileSpacing = 'compact';
+%t2.TileSpacing = 'compact';
 t2.Padding = 'compact';
-title(t2,"Experimental");
+title(t2,"Steady State Temperatures along the Rod",'FontSize',16);
+
 
 %% Go through each file
 for i=1:length(filename)
     figure(1);
     nexttile; hold on; grid on; grid minor;
-    set(gca,"ColorOrder",parula(16))
+    %set(gca,"ColorOrder",parula(16))
     titleFile = char (filename(i)); % Filename for the data
     % Voltage and Current
     if (i==1 || i==2)
@@ -33,9 +34,9 @@ for i=1:length(filename)
 
     rawData = importdata(filename(i));
     testData = rawData.data;
-    plot(testData(:,1),testData(:,2:end),LineWidth=2);
-    xlabel("time [s]");
-    ylabel("temperature ["+char(176)+"C]")
+
+    xlabel("Location along Rod [m]",'FontSize',14);
+    ylabel("Temperature ["+char(176)+"C]",'FontSize',14)
     
     if (i==1 || i==2)
         title(titleFile(1,1:8)+" "+volt+"V, "+curr+"mA",'FontSize',14);
@@ -49,7 +50,8 @@ for i=1:length(filename)
     x_0 = (1+3/8)*0.0254;% Distance from x_0 to first thermocouple
     spacing = 0.5*0.0254; % Distance between thermocouples
     pos_therm = linspace(x_0,x_0+(8*spacing),8); % [m]
-
+    scatter(pos_therm,T_F,25,'r','filled');
+    
     Coeff = polyfit(pos_therm,T_F,1);
     H_exp(i) = Coeff(1); % [C/m]
     T_0(i) = Coeff(2); % [C]
@@ -57,9 +59,12 @@ for i=1:length(filename)
     v_exp = T_0(i) + H_exp(i)*pos_therm;
     v_an = T_0(i) + H_an(i)*pos_therm;
     [M,N] = size(testData(:,1));
-    plot(testData(:,1), v_exp.*ones(M,1),LineWidth=2)
+    plot(pos_therm,H_exp(i)*pos_therm+Coeff(2),'r',LineWidth=2);
+    plot(pos_therm,H_exp(i)*pos_therm+Coeff(2)+2,'--r',LineWidth=1);
+    plot(pos_therm,H_exp(i)*pos_therm+Coeff(2)-2,'--r',LineWidth=1);
+    plot(pos_therm,H_an(i)*pos_therm+Coeff(2),'b',LineWidth=2);
 end
 ax = nexttile(1);
-lg  = legend('Ch1','Ch2','Ch3','Ch4','Ch5','Ch6','Ch7','Ch8',' ',' ',' ',' ',' ',' ',' ',' ',' ','Orientation','Vertical','NumColumns',2, 'Interpreter', 'Latex');
+lg  = legend('Experimental Data','H_e_x_p (Experimental Slope)','Thermocouple error','','H_a_n (Analytical Slope)','Orientation','Vertical','FontSize',13);
 lg.Layout.Tile = 6;
 
