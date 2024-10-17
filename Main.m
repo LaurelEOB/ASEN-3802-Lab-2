3e0b65c0a6e4eb3920875ccf8f2155e297e9d291
jsadow9446 <jsadow9446@gmail.com>
2024-10-17 14:37:12 -0600

    Update Main.m
    FIxed the function for p2t2

--- Main.m
+++ Main.m
@@ -143,3 +143,28 @@ end
 % Adds the transient values for each x and t. 
 u(2:end,:) = T_0 + H.*x + total;
 end
+% This version of the function is for plotting u as x and t change, with a fixed value of n_max. Used for p2t2.
+function u = transientTemp(x,t,T_0,H,L,k,rho,c_p,n_max)
+total = zeros(length(t),length(x)); % Sets the initial transient state term (n=0) to be 0
+alpha = k./(rho.*c_p); % Gets alpha
+u = zeros(length(t),length(x)); % Pre-allocates u for speed
+%u(1,:) = T_0 + H.*x; % Establishes n=0 as the steady state case.
+for n = 1:n_max % Loops through values of n
+    n;
+    lambda = ((2.*n-1)*pi)./(2.*L); % Calculates lambda
+    % The below calculates b
+    if mod(n,2) == 0 % If n is even
+        b = (8.*H.*L)./((2.*n-1).^2.*pi.^2);
+    else
+        b = (-8.*H.*L)./((2.*n-1).^2.*pi.^2);
+    end
+    % Updates the sum for each value of n
+    total = total + b.*sin(lambda.*x).*exp(-1.*lambda.^2.*alpha.*t);
+    % Gives a u value for each value of n
+    
+end
+% Adds the transient values for each x and t. 
+u(1:end,:) = T_0 + H.*x + total;
+%u(1,:) = T_0 + H.*x; % Establishes n=0 as the steady state case.
+end
+
